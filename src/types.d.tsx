2890b14fd2e6f7703d5364d7d8084d3e163b22f2
Danny Fuhriman <danny.fuhriman@Okta.com>
2022-01-07 10:31:27 -0800

    create common colors type

--- src/types.d.tsx
+++ src/types.d.tsx
@@ -4,3 +4,12 @@ interface MaterialComponentProps {
 	className?: string;
 	children?: React.ReactNode;
 }
+
+type Colors =
+	| 'primary'
+	| 'warning'
+	| 'danger'
+	| 'success'
+	| 'info'
+	| 'rose'
+	| 'gray';
