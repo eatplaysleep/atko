ca841d0fbf238d822a2ac01d0c2108c2e0ff4835
Danny Fuhriman <danny.fuhriman@Okta.com>
2022-01-10 20:05:21 -0500

    add fontStyle

--- src/styles/jss/fonts.tsx
+++ src/styles/jss/fonts.tsx
@@ -1,12 +1,20 @@
 /** @format */
 import { createStyles } from '@material-ui/core/styles';
 
-const title = {
-	color: '#3C4858',
-	margin: '1.75rem 0 0.875rem',
-	textDecoration: 'none',
-	fontWeight: 700,
-	fontFamily: `"Roboto Slab", "Times New Roman", serif`,
-};
+export const fontStyle = createStyles({
+	default: {
+		fontFamily: `"Roboto", "Helvetica", "Arial", sans-serif`,
+		fontWeight: 300,
+		lineHeight: '1.5em',
+	},
+});
 
-export const titleStyle = createStyles({ title });
+export const titleStyle = createStyles({
+	title: {
+		color: '#3C4858',
+		margin: '1.75rem 0 0.875rem',
+		textDecoration: 'none',
+		fontWeight: 700,
+		fontFamily: `"Roboto Slab", "Times New Roman", serif`,
+	},
+});
